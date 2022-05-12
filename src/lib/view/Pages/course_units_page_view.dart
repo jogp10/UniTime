ad5f083d2e03aa3321598935e09a111bd646251d
RicardoCavalheiro123 <ricardo18-4-2001@hotmail.com>
2022-05-12 15:55:57 +0100

    Course units

--- src/lib/view/Pages/course_units_page_view.dart
+++ src/lib/view/Pages/course_units_page_view.dart
@@ -18,40 +18,26 @@ class CourseUnitsPageView extends StatefulWidget {
 
 /// Manages the 'UCs' section of the app.
 class CourseUnitsPageViewState extends SecondaryPageViewState {
-}
-
-class CourseUnitsList extends StatelessWidget {
-  final List<CourseUnit> courseUnits;
-
-  CourseUnitsList({Key key, @required this.courseUnits}) : super(key: key);
+  List<String> options = ['Base de Dados', 'Sistemas Operativos'];
 
   @override
-  Widget build(BuildContext context) {
+  Widget getBody(BuildContext context) {
     return Scaffold(
-      body: BackButtonExitWrapper(
-        context: context,
-        child: createScrollableCardView(context),
+      body: ListView.builder(
+          itemCount: options.length,
+          itemBuilder: (context, index) {
+            return Card(
+                child: ListTile(
+                  onTap: () {},
+                  title: Text(options[index]),
+                  textColor: Colors.blue,
+                )
+            );
+          }
       ),
     );
   }
-
-  Widget createScrollableCardView(BuildContext context) {
-    List<CourseUnit> courseUnits;
-    return null;
-  }
-
-  List<Widget> createUnitsWidget(List<CourseUnit> courseUnits,
-      BuildContext context) {
-    if (courseUnits == null) return [];
-
-    final List<Widget> result = <Widget>[];
-    for (int i = 0; i < courseUnits.length; i++) {
-      result.add(this.createUnitWidget(courseUnits[i], i, context));
-    }
-    return result;
-  }
-
-  Widget createUnitWidget(CourseUnit courseUnit, int i, BuildContext context) {
-    return null;
-  }
 }
+
+
+
